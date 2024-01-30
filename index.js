1d07c94be080a2011f0ff0dde3b14550598572c8
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2024-01-30 13:55:44 -0500

    1/30/24 deploying some tests and seeing if contact form works on Netlify

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+$(function (){
+  $(".tech-icon").on("mouseover", (event)=> {
+    $(event.target).addClass("tech-icon-pop");
+  });
+  $(".tech-icon").on("mouseout", (event)=> {
+    $(event.target).removeClass("tech-icon-pop");
+  });
+});
