be9824a1f2fa0a83f0e28869da6c38c52abaf469
Gabe-Fernandes <gabrielcatanifernandes@gmail.com>
2024-02-01 09:52:24 -0500

    2/1/24 Matrix multiplication feature started, first draft color palette introduced, and btn styles added.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 $(function (){
   $(".tech-icon").on("mouseover", (event)=> {
     $(event.target).addClass("tech-icon-pop");
+    if ($(event.target).parent().hasClass("tech-icon")) {
+      $(event.target).parent().addClass("tech-icon-pop");
+    }
   });
   $(".tech-icon").on("mouseout", (event)=> {
     $(event.target).removeClass("tech-icon-pop");
+    if ($(event.target).parent().hasClass("tech-icon")) {
+      $(event.target).parent().removeClass("tech-icon-pop");
+    }
   });
 });
